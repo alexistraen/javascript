ec9db584d7fc1bb763d2bdd196ba2bc1481c8e14
alexistraen <alexis.traen@gmail.com>
2020-11-09 20:53:27 +0100

    Exo1 update

--- P6-Boucles/Exo1/assets/js/main.js
+++ P6-Boucles/Exo1/assets/js/main.js
@@ -4,6 +4,6 @@ for (let yearsLoop = 2020; yearsLoop <= 2030; yearsLoop++) {
     let redSpan = document.createElement('span');
     yearsList.appendChild(redSpan).innerHTML = yearsLoop + '<br>';
     if ((yearsLoop%4==0) && ((yearsLoop%100!=0) || (yearsLoop%400==0))) {
-        redSpan.classList.add('red');
+        redSpan.classList.add('redSpan');
     }
 }
